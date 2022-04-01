34f684835c619e28e32b196088fc70bdd4a2a650
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-04-01 11:04:45 -0300

    quotes details styles implemented

---
+++ ui/src/atomic/components/quotes-layout/current-quote-details-card.tsx
@@ -0,0 +1,53 @@
+import { DL, DT, Separator, DD, Box } from '@atomic/shared'
+import { QuoteDataModel } from '@entities/quote'
+import { FC } from 'react'
+
+interface ICurrentQuoteDetailsCard {
+    quote: QuoteDataModel
+}
+
+export const CurrentQuoteDetailsCard: FC<ICurrentQuoteDetailsCard> = ({ quote }) => (
+    <DL fluid>
+        <Box horizontal fluid>
+            <DT isBolded>status:</DT>
+            <Separator vertical />
+            <DD>{quote.statusCurrent}</DD>
+            <Separator vertical large />
+            <DT isBolded>price:</DT>
+            <Separator vertical />
+            <DD>$ {quote.priceFinal}</DD>
+        </Box>
+        <Box horizontal fluid>
+            <DT isBolded>contact:</DT>
+            <Separator vertical />
+            <DD>{quote.peopleContact}</DD>
+            <Separator vertical large />
+            <DT isBolded>people:</DT>
+            <Separator vertical />
+            <DD>{quote.peopleCount}</DD>
+        </Box>
+        <Box horizontal fluid>
+            <DT isBolded>departure:</DT>
+            <Separator vertical />
+            <DD>{quote.departureDate}</DD>
+            <Separator vertical large />
+            <DT isBolded>location:</DT>
+            <Separator vertical />
+            <DD>{quote.departureLocation}</DD>
+        </Box>
+        <Box horizontal fluid>
+            <DT isBolded>return:</DT>
+            <Separator vertical />
+            <DD>{quote.returnDate}</DD>
+            <Separator vertical large />
+            <DT isBolded>location:</DT>
+            <Separator vertical />
+            <DD>{quote.destinationLocation}</DD>
+        </Box>
+        <Box horizontal fluid>
+            <DT isBolded>transportation:</DT>
+            <Separator vertical />
+            <DD>{quote.transportationType}</DD>
+        </Box>
+    </DL>
+)
--- ui/src/atomic/components/quotes-layout/current-quote-details-view.tsx
+++ ui/src/atomic/components/quotes-layout/current-quote-details-view.tsx
@@ -1,14 +1,18 @@
+import { Paragraph } from '@atomic/shared'
 import { QuoteDataModel } from '@entities/quote'
 import { FC } from 'react'
 import { CardWithActions } from '../common-layout'
+import { CurrentQuoteDetailsCard } from './current-quote-details-card'
 
 interface ICurrentQuoteDetailsView {
     quote: QuoteDataModel
 }
 
 export const CurrentQuoteDetailsView: FC<ICurrentQuoteDetailsView> = ({ quote }) => (
-    <CardWithActions iconMain='Quote' title={`Quote ID: ${quote?.id || ''}`} >
-        Quote infos here
+    <CardWithActions iconMain="Quote" title={`Quote ID: ${quote?.id || ''}`}>
+        {quote ? <CurrentQuoteDetailsCard quote={quote} /> :
+            <Paragraph>No detailed data. Select one in all quotes</Paragraph>
+        }
     </CardWithActions>
 )
 
