b0ad9aecc18d370f4c50851885c58be4ab2e03e9
OsmanRodrigues <osmancesar.mr@gmail.com>
2022-04-01 00:18:45 -0300

    card with actions refactor

--- ui/src/atomic/components/home-layout/quote-section/pending-quote-view.tsx
+++ ui/src/atomic/components/home-layout/quote-section/pending-quote-view.tsx
@@ -3,7 +3,7 @@ import { FC, useEffect } from 'react'
 import { observer } from 'mobx-react-lite'
 import { quoteStore } from '@domain/quote-domain/quote-store'
 import { PendingQuoteTable } from './pending-quote-table'
-import { QuoteView } from './quote-view'
+import { CardWithActions } from '../../common-layout'
 
 export const PendingQuoteView: FC = observer(() => {
     const hasData = !!quoteStore.quotes?.length
@@ -13,8 +13,8 @@ export const PendingQuoteView: FC = observer(() => {
     }, [])
 
     return (
-        <QuoteView iconMain='Pending' title="Pending quotes" handleRefresh={quoteStore.getQuotes}>
+        <CardWithActions iconMain='Pending' title="Pending quotes" handleRefresh={quoteStore.getQuotes}>
             {hasData ? <PendingQuoteTable /> : <H4>No pending quotes info.</H4>}
-        </QuoteView>
+        </CardWithActions>
     )
 })
